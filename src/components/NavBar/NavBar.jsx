79439c9f50feceb8367618a88e8a7f77179434e3
Jos4ma <88600626+Jos4ma@users.noreply.github.com>
2022-08-03 19:47:24 -0400

    separation button and color

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -31,12 +31,12 @@ export default function LandingPage() {
             <SearchBtn/>
 
           <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-            <a href="#" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+            <a href="#" className="whitespace-nowrap text-base font-medium text-gray-400 hover:text-black">
               Sign in
             </a>
             <a
               href="#"
-              className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-[#1f2937] hover:bg-[#cbd5e1]"
+              className="ml-4 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-[#1f2937] hover:bg-[#cbd5e1]"
             >
               Sign up
             </a>
