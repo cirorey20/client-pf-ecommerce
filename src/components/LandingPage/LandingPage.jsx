363f9abe390534e4795f90f5d2daf51dcce613c9
Alek30 <alemank23k@gmail.com>
2022-08-04 14:06:09 -0300

    add component change Tendencia

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -4,12 +4,14 @@ import style from "./LandingPage.module.css";
 import NavBar from "../NavBar/NavBar";
 import Footer from "../Footer/Footer.jsx";
 import SliderShow from "../SliderShow/SliderShow";
+import Tendencia from "../Tendencia/Tendencia";
 
 export default function LandingPage() {
   return (
     <div className="flex flex-col min-h-screen">
       <NavBar />
       <SliderShow />
+      <Tendencia />
       <Footer />
     </div>
   );
