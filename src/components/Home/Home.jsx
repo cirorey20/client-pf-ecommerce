773adb7458c102f41ab40020b180fe9a1c93c8b1
cirorey20 <cirorey95@gmail.com>
2022-08-03 21:16:51 -0300

    feat(component/Home): add button cart

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import {useSelector, useDispatch} from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { getProducts } from '../../redux/actions/products';
 import { useEffect } from "react";
 
@@ -13,44 +13,60 @@ const Home = () => {
         console.log(allProducts);
     }, [dispatch])
 
-    
-    return(
+
+    return (
         <Fragment>
             <h1 className="text-6xl">Universal Music</h1>
             <br />
             <br />
 
-            <div className="md:container md:mx-auto">    
+            <div className="md:container md:mx-auto bg-[#e2e8f0]">
                 <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+
                     {
-                        allProducts.map((e, i) => {
-                            return(
-                                <div key={i} className="group relative m-5">
 
-                                    <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-                                        <img
-                                        src={e.image}
-                                        alt="NOT_FOUND"
-                                        className="w-full h-full object-center object-cover lg:w-full lg:h-full"
-                                        />
-                                    </div>
+                        allProducts.length <= 0 ?
+                            <div>NO HAY PRODUCTOS...</div>
+                            :
+                            allProducts.map((e, i) => {
+                                if (e.enable === true) {
+                                    return (
+                                        <div key={i} className="  ">
+                                            <div className="relative m-5 group ">
 
-                                    <div className="mt-4 flex justify-between">
-                                        <div>
-                                        <h3 className="text-sm text-gray-700">
-                                            <a href="#">
-                                                <span aria-hidden="true" className="absolute inset-0" />
-                                                {e.name}
-                                            </a>
-                                        </h3>
-                                        <p className="mt-1 text-sm text-gray-500">Stock</p>
+                                                <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+                                                    <img
+                                                        src={e.image}
+                                                        alt="NOT_FOUND"
+                                                        className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+                                                    />
+                                                </div>
+        
+                                                <div className="mt-4 flex justify-between">
+                                                    <div>
+                                                        <h3 className="text-sm text-gray-700">
+                                                            <a href="#">
+                                                                <span aria-hidden="true" className="absolute inset-0" />
+                                                                {e.name } 
+                                                            </a>
+                                                            
+                                                        </h3>
+                                                        <p className="mt-1 text-sm text-gray-500">Stock {e.stock}</p>
+                                                    </div>
+                                                    <p className="text-sm font-medium text-gray-900">${e.price}</p>
+                                                    
+                                                </div>
+                                            </div>
+                                            <button class="mb-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                                Add Cart
+                                            </button>
                                         </div>
-                                        <p className="text-sm font-medium text-gray-900">${e.price}</p>
-                                    </div>
-                                </div>
-                            )
-                        })
-                    }
+                                    )
+                                } 
+                                
+                            })
+                        }
+
                 </div>
             </div>
         </Fragment>
@@ -58,3 +74,34 @@ const Home = () => {
 }
 
 export default Home;
+
+/*
+    allProducts.map((e, i) => {
+        return(
+            <div key={i} className="group relative m-5">
+
+                <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+                    <img
+                    src={e.image}
+                    alt="NOT_FOUND"
+                    className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+                    />
+                </div>
+
+                <div className="mt-4 flex justify-between">
+                    <div>
+                    <h3 className="text-sm text-gray-700">
+                        <a href="#">
+                            <span aria-hidden="true" className="absolute inset-0" />
+                            {e.name}
+                        </a>
+                    </h3>
+                    <p className="mt-1 text-sm text-gray-500">Stock</p>
+                    </div>
+                    <p className="text-sm font-medium text-gray-900">${e.price}</p>
+                </div>
+            </div>
+        )
+    })
+
+*/
