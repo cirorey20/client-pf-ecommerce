16175f3697dc06192b5e93087f13d6baf1783deb
Alek30 <alemank23k@gmail.com>
2022-08-05 15:52:42 -0300

    change class/className

--- src/components/Tendencia/Tendencia.jsx
+++ src/components/Tendencia/Tendencia.jsx
@@ -4,32 +4,32 @@ import "flowbite";
 export default function Tendencia() {
   return (
     <div className="md:container md:mx-auto bg-[#e2e8f0]">
-      <div class="relative flex py-10 items-center border-gray-200 shadow-md">
-        <div class="flex-grow border-t border-gray-400"></div>
-        <span class="flex-shrink mx-4 px-3 text-xl text-gray-400">
+      <div className="relative flex py-10 items-center border-gray-200 shadow-md">
+        <div className="flex-grow border-t border-gray-400"></div>
+        <span className="flex-shrink mx-4 px-3 text-xl text-gray-400">
           Tendencia
         </span>
-        <div class="flex-grow border-t border-gray-400 "></div>
+        <div className="flex-grow border-t border-gray-400 "></div>
       </div>
       <div className="md:container md:mx-auto mt-10 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 ">
-        <div class="max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div className="max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
           <a href="#">
             <img
-              class="rounded-t-lg"
+              className="rounded-t-lg"
               src="https://cdn.shopify.com/s/files/1/0253/8618/8834/products/fee42e7ae64f_1280x.jpg?v=1653757251"
               alt="product image"
             />
           </a>
-          <div class="p-5 pb-4 mt-2">
+          <div className="p-5 pb-4 mt-2">
             <a href="#">
-              <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+              <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
                 Guitarras
               </h5>
             </a>
-            <div class="flex items-center mt-2.5 mb-5">
+            <div className="flex items-center mt-2.5 mb-5">
               <svg
                 aria-hidden="true"
-                class="w-5 h-5 text-yellow-300"
+                className="w-5 h-5 text-yellow-300"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -39,7 +39,7 @@ export default function Tendencia() {
               </svg>
               <svg
                 aria-hidden="true"
-                class="w-5 h-5 text-yellow-300"
+                className="w-5 h-5 text-yellow-300"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -49,7 +49,7 @@ export default function Tendencia() {
               </svg>
               <svg
                 aria-hidden="true"
-                class="w-5 h-5 text-yellow-300"
+                className="w-5 h-5 text-yellow-300"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -59,7 +59,7 @@ export default function Tendencia() {
               </svg>
               <svg
                 aria-hidden="true"
-                class="w-5 h-5 text-yellow-300"
+                className="w-5 h-5 text-yellow-300"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -69,7 +69,7 @@ export default function Tendencia() {
               </svg>
               <svg
                 aria-hidden="true"
-                class="w-5 h-5 text-yellow-300"
+                className="w-5 h-5 text-yellow-300"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -77,41 +77,41 @@ export default function Tendencia() {
                 <title>Fifth star</title>
                 <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
               </svg>
-              <span class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
+              <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
                 5.0
               </span>
             </div>
-            <div class="flex justify-between items-center">
-              <span class="text-3xl font-bold text-gray-900 dark:text-white">
+            <div className="flex justify-between items-center">
+              <span className="text-3xl font-bold text-gray-900 dark:text-white">
                 $599
               </span>
               <a
                 href="#"
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
                 Add to cart
               </a>
             </div>
           </div>
         </div>
-        <div class="max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div className="max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
           <a href="#">
             <img
-              class="rounded-t-lg"
+              className="rounded-t-lg"
               src="https://www.audiomundo.cl/images/item//1/0/1086375_1.jpg"
               alt="product image"
             />
           </a>
-          <div class="p-5 pb-4 mt-2">
+          <div className="p-5 pb-4 mt-2">
             <a href="#">
-              <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+              <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
                 Guitarras eléctricas
               </h5>
             </a>
-            <div class="flex items-center mt-2.5 mb-5">
+            <div className="flex items-center mt-2.5 mb-5">
               <svg
                 aria-hidden="true"
-                class="w-5 h-5 text-yellow-300"
+                className="w-5 h-5 text-yellow-300"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -121,7 +121,7 @@ export default function Tendencia() {
               </svg>
               <svg
                 aria-hidden="true"
-                class="w-5 h-5 text-yellow-300"
+                className="w-5 h-5 text-yellow-300"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -131,7 +131,7 @@ export default function Tendencia() {
               </svg>
               <svg
                 aria-hidden="true"
-                class="w-5 h-5 text-yellow-300"
+                className="w-5 h-5 text-yellow-300"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -141,7 +141,7 @@ export default function Tendencia() {
               </svg>
               <svg
                 aria-hidden="true"
-                class="w-5 h-5 text-yellow-300"
+                className="w-5 h-5 text-yellow-300"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -151,7 +151,7 @@ export default function Tendencia() {
               </svg>
               <svg
                 aria-hidden="true"
-                class="w-5 h-5 text-yellow-300"
+                className="w-5 h-5 text-yellow-300"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -159,41 +159,41 @@ export default function Tendencia() {
                 <title>Fifth star</title>
                 <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
               </svg>
-              <span class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
+              <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
                 5.0
               </span>
             </div>
-            <div class="flex justify-between items-center">
-              <span class="text-3xl font-bold text-gray-900 dark:text-white">
+            <div className="flex justify-between items-center">
+              <span className="text-3xl font-bold text-gray-900 dark:text-white">
                 $599
               </span>
               <a
                 href="#"
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
                 Add to cart
               </a>
             </div>
           </div>
         </div>
-        <div class="max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div className="max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
           <a href="#">
             <img
-              class="rounded-t-lg"
+              className="rounded-t-lg"
               src="https://cdn.shopify.com/s/files/1/0253/8618/8834/products/516d24e74a86_6beb56f3-568f-41d8-96a6-00361fc05582_1280x.jpg?v=1617839275"
               alt="product image"
             />
           </a>
-          <div class="p-5 pb-4 mt-2">
+          <div className="p-5 pb-4 mt-2">
             <a href="#">
-              <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+              <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
                 Baterías
               </h5>
             </a>
-            <div class="flex items-center mt-2.5 mb-5">
+            <div className="flex items-center mt-2.5 mb-5">
               <svg
                 aria-hidden="true"
-                class="w-5 h-5 text-yellow-300"
+                className="w-5 h-5 text-yellow-300"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -203,7 +203,7 @@ export default function Tendencia() {
               </svg>
               <svg
                 aria-hidden="true"
-                class="w-5 h-5 text-yellow-300"
+                className="w-5 h-5 text-yellow-300"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -213,7 +213,7 @@ export default function Tendencia() {
               </svg>
               <svg
                 aria-hidden="true"
-                class="w-5 h-5 text-yellow-300"
+                className="w-5 h-5 text-yellow-300"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -223,7 +223,7 @@ export default function Tendencia() {
               </svg>
               <svg
                 aria-hidden="true"
-                class="w-5 h-5 text-yellow-300"
+                className="w-5 h-5 text-yellow-300"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -233,7 +233,7 @@ export default function Tendencia() {
               </svg>
               <svg
                 aria-hidden="true"
-                class="w-5 h-5 text-yellow-300"
+                className="w-5 h-5 text-yellow-300"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -241,41 +241,41 @@ export default function Tendencia() {
                 <title>Fifth star</title>
                 <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
               </svg>
-              <span class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
+              <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
                 5.0
               </span>
             </div>
-            <div class="flex justify-between items-center">
-              <span class="text-3xl font-bold text-gray-900 dark:text-white">
+            <div className="flex justify-between items-center">
+              <span className="text-3xl font-bold text-gray-900 dark:text-white">
                 $599
               </span>
               <a
                 href="#"
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
                 Add to cart
               </a>
             </div>
           </div>
         </div>
-        <div class="max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div className="max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
           <a href="#">
             <img
-              class="p-0 rounded-t-lg"
+              className="p-0 rounded-t-lg"
               src="http://images.musicstore.de/images/1280/roland-v-piano-grand-gp-7-pe-piano-de-cola-con-physical-modelling_1_EPI0000609-000.jpg"
               alt="product image"
             />
           </a>
-          <div class="p-5 pb-4 mt-2">
+          <div className="p-5 pb-4 mt-2">
             <a href="#">
-              <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+              <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
                 Piano
               </h5>
             </a>
-            <div class="flex items-center mt-2.5 mb-5">
+            <div className="flex items-center mt-2.5 mb-5">
               <svg
                 aria-hidden="true"
-                class="w-5 h-5 text-yellow-300"
+                className="w-5 h-5 text-yellow-300"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -285,7 +285,7 @@ export default function Tendencia() {
               </svg>
               <svg
                 aria-hidden="true"
-                class="w-5 h-5 text-yellow-300"
+                className="w-5 h-5 text-yellow-300"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -295,7 +295,7 @@ export default function Tendencia() {
               </svg>
               <svg
                 aria-hidden="true"
-                class="w-5 h-5 text-yellow-300"
+                className="w-5 h-5 text-yellow-300"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -305,7 +305,7 @@ export default function Tendencia() {
               </svg>
               <svg
                 aria-hidden="true"
-                class="w-5 h-5 text-yellow-300"
+                className="w-5 h-5 text-yellow-300"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -315,7 +315,7 @@ export default function Tendencia() {
               </svg>
               <svg
                 aria-hidden="true"
-                class="w-5 h-5 text-yellow-300"
+                className="w-5 h-5 text-yellow-300"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -323,17 +323,17 @@ export default function Tendencia() {
                 <title>Fifth star</title>
                 <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
               </svg>
-              <span class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
+              <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
                 5.0
               </span>
             </div>
-            <div class="flex justify-between items-center">
-              <span class="text-3xl font-bold text-gray-900 dark:text-white">
+            <div className="flex justify-between items-center">
+              <span className="text-3xl font-bold text-gray-900 dark:text-white">
                 $599
               </span>
               <a
                 href="#"
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
                 Add to cart
               </a>
