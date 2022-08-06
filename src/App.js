28f93be0161e47d324d56c5921a115e2f30f66a8
Alek30 <alemank23k@gmail.com>
2022-08-06 14:00:08 -0300

    delete path

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import './App.css';
-import {Routes, Route} from 'react-router-dom'
-import LandingPage from './components/LandingPage/LandingPage'
-import Home from './components/Home/Home';
-import Details from './components/Details/Details';
-import ViewCart from './components/Cart/ViewCart';
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage/LandingPage";
+import Home from "./components/Home/Home";
+import Details from "./components/Details/Details";
+import ViewCart from "./components/Cart/ViewCart";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route exact path='/' element={<LandingPage/>}/>
-        <Route path='/home' element={<Home/>}/>
-        <Route path='/product/:id' element={<Details/>}/> 
-        <Route path='/cart' element={<ViewCart/>}/> 
+        <Route exact path="/" element={<LandingPage />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/product/:id" element={<Details />} />
+        <Route path="/cart" element={<ViewCart />} />
       </Routes>
     </div>
   );
