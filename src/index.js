441465a53169f02a4b645cabbe4cbf22fc3144f1
Alek30 <alemank23k@gmail.com>
2022-08-05 22:50:39 -0300

    add Auth0Provider

--- src/index.js
+++ src/index.js
@@ -1,31 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import store from './redux/store/index'
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import store from "./redux/store/index";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
-        <App />
+        <Auth0Provider
+          domain={domain}
+          clientId={clientId}
+          redirectUri={window.location.origin}
+        >
+          <App />
+        </Auth0Provider>
       </BrowserRouter>
-    </React.StrictMode>,
+    </React.StrictMode>
+    ,
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-
-
-
 
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
@@ -35,8 +42,6 @@ reportWebVitals();
 // import store from './store'
 // import { Provider } from 'react-redux';
 // import { BrowserRouter } from 'react-router-dom'
-
-
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
