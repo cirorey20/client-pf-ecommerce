0914cba89a1e01bfd2ab47d6f50cb0837b8f19b6
Ariakis <ariakisb@.gmail.com>
2022-08-07 13:37:09 -0500

    searchBar

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -17,7 +17,6 @@ const Home = () => {
     const allProducts = useSelector((state) => state.productReducer.products)
     const allCategories = useSelector((state) => state.categoryReducer.categories)
     const paginaActual = useSelector((state) => state.productReducer.paginaActual)
-    //console.log(paginaActual)
     
     //paginado
     //const [paginaActual, setPaginaActual] = useState(1)
