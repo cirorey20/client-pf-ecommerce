86e37b99dc402d6c31e2d8747a88ab212e0e97e2
Alek30 <alemank23k@gmail.com>
2022-08-07 23:58:32 -0300

    add path UserInfo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage/LandingPage";
 import Home from "./components/Home/Home";
 import Details from "./components/Details/Details";
+import UserInfo from "./components/Profile/UserInfo";
 import ViewCart from "./components/Cart/ViewCart";
 import CreateProduct from "./components/CreateProduct/CreateProduct";
 
@@ -13,6 +14,7 @@ function App() {
         <Route exact path="/" element={<LandingPage />} />
         <Route path="/home" element={<Home />} />
         <Route path="/product/:id" element={<Details />} />
+        <Route path="/logged/userInfo" element={<UserInfo />} />
         <Route path="/cart" element={<ViewCart />} />
         <Route path="/product/create" element={<CreateProduct />} />
         <Route path="/product/update/:idProduct" element={<CreateProduct />} />
