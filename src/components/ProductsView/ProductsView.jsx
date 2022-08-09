0617a158af2dea1994ff5dedc0d5eaf180315106
Alek30 <alemank23k@gmail.com>
2022-08-09 00:20:14 -0300

    change hover:shadow

--- src/components/ProductsView/ProductsView.jsx
+++ src/components/ProductsView/ProductsView.jsx
@@ -3,9 +3,9 @@ import "flowbite";
 
 export default function ProductsView() {
   return (
-    <div className="flex space-x-4 md:container md:mx-auto bg-[#e2e8f0]">
-      <div className="min-h-screen bg-gray-100 flex justify-center items-center">
-        <div className="w-70 rounded-md cursor-pointer shadow-lg overflow-hidden hover:shadow-xl transform hover:scale-105 duration-500">
+    <div className="flex space-x-5 md:container md:mx-auto mb-12 h-[68vh] bg-[#fbfbfccb] shadow-gray-900">
+      <div className="min-h-screen bg-[#e2e8f0]flex pt-20">
+        <div className="border-2 w-70 h-min rounded-md cursor-pointer shadow-lg overflow-hidden hover:shadow-xl transform hover:scale-105 duration-500">
           <img
             src="https://i.pinimg.com/originals/b7/4e/d1/b74ed19a92a174d85c3fa1879b1e361e.jpg"
             alt=""
@@ -18,8 +18,8 @@ export default function ProductsView() {
           </div>
         </div>
       </div>
-      <div className="min-h-screen bg-gray-100 flex justify-center items-center">
-        <div className="w-70 rounded-md cursor-pointer shadow-lg overflow-hidden hover:shadow-xl transform hover:scale-105 duration-500">
+      <div className="min-h-screen bg-[#e2e8f0]flex pt-20">
+        <div className="border-2 w-70 h-min rounded-md cursor-pointer shadow-lg overflow-hidden hover:shadow-xl transform hover:scale-105 duration-500">
           <img
             src="https://ih1.redbubble.net/image.2245188445.9470/st,small,845x845-pad,1000x1000,f8f8f8.jpg"
             alt=""
@@ -32,8 +32,8 @@ export default function ProductsView() {
           </div>
         </div>
       </div>
-      <div className="min-h-screen bg-gray-100 flex justify-center items-center">
-        <div className="w-70 rounded-md cursor-pointer shadow-lg overflow-hidden hover:shadow-xl transform hover:scale-105 duration-500">
+      <div className="min-h-screen bg-[#e2e8f0]flex pt-20">
+        <div className="border-2 w-70 h-min rounded-md cursor-pointer shadow-lg overflow-hidden hover:shadow-xl transform hover:scale-105 duration-500">
           <img
             src="https://cdn1.coppel.com/images/catalog/mkp/147/3000/1471001-1.jpg"
             alt=""
@@ -46,8 +46,8 @@ export default function ProductsView() {
           </div>
         </div>
       </div>
-      <div className="min-h-screen bg-gray-100 flex justify-center items-center">
-        <div className="w-70 rounded-md cursor-pointer shadow-lg overflow-hidden hover:shadow-xl transform hover:scale-105 duration-500">
+      <div className="min-h-screen bg-[#e2e8f0]flex pt-20">
+        <div className="border-2 w-70 h-min rounded-md cursor-pointer shadow-lg overflow-hidden hover:shadow-xl transform hover:scale-105 duration-500">
           <img
             src="https://ih1.redbubble.net/image.3051489577.3866/poster,840x830,f8f8f8-pad,1000x1000,f8f8f8.jpg"
             alt=""
