ec4b8c7e86b33e792631b17de43d87bb46db108f
Ariakis <ariakisb@.gmail.com>
2022-08-10 20:06:47 -0500

    forms

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage/LandingPage";
@@ -7,10 +8,23 @@ import UserInfo from "./components/Profile/UserInfo";
 import ViewCart from "./components/Cart/ViewCart";
 import CreateProduct from "./components/CreateProduct/CreateProduct";
 import DashBoard from "./components/DashBoard/DashBoard.jsx";
+////////////////////////////////////////////////////
+import {loadStripe} from '@stripe/stripe-js';
+import {Elements} from '@stripe/react-stripe-js';
+import CheckoutForm from "../src/components/CheckoutForm/checkoutForm"
+const stripePromise = loadStripe("pk_test_51LVJYJHeLDBhzI8Lq31jc8ysAX8rt772XLyBE6QcT2RSHemM1qKzefj0dtCwowXnfIrvwUcDpV5L75iKMGjCVQEm00vRY3grND")
+
+
 
 function App() {
   return (
-    <div className="App">
+    
+    <Elements stripe={stripePromise}>
+        <CheckoutForm/>
+    </Elements>
+    
+    /* <div className="App">
+    
       <Routes>
         <Route exact path="/" element={<LandingPage />} />
         <Route path="/home" element={<Home />} />
@@ -22,6 +36,7 @@ function App() {
         <Route path="/product/DashBoard" element={<DashBoard />} />
       </Routes>
     </div>
+ */
   );
 }
 
---
+++ src/components/CheckoutForm/checkoutForm.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+//import {loadStripe} from '@stripe/stripe-js';
+import {CardElement, useStripe, useElements} from '@stripe/react-stripe-js';
+import axios from "axios";
+
+
+//const stripePromise = loadStripe("pk_test_51LVJYJHeLDBhzI8Lq31jc8ysAX8rt772XLyBE6QcT2RSHemM1qKzefj0dtCwowXnfIrvwUcDpV5L75iKMGjCVQEm00vRY3grND")
+
+
+const CheckoutForm = () => {
+
+    const stripe = useStripe()
+    const element = useElements()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const {error, paymentMethod} = await stripe.createPaymentMethod({
+            type: "card",
+            card: element.getElement(CardElement)
+        })
+        if(!error){
+            
+            const {id} = paymentMethod
+
+            const {data} = await axios.post('http://localhost:3001/api/checkout',{
+                id,
+                amount: 1000
+            });
+            console.log(data);
+            console.log(paymentMethod)
+        }
+    }
+    return (
+    <form onSubmit={handleSubmit} >
+         <CardElement/>
+         <button>Buy</button>
+    </form>
+    );
+};
+
+
+export default CheckoutForm;
