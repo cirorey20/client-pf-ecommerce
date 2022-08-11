e769e3d568f6571d596bc242f5e774d2ce7bdc27
cirorey20 <cirorey95@gmail.com>
2022-08-11 16:28:21 -0300

    changes for deploy

--- src/config/config.js
+++ src/config/config.js
@@ -8,7 +8,8 @@
 // export {
 //     config
 // };
-const URL_API = "http://localhost:3001/api/v1/"
+// const URL_API = "http://localhost:3001/api/v1/"
+const URL_API = "https://universalmusic.herokuapp.com/api/v1/"
 
 export {
     URL_API
