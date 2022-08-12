7981cae3fe7b72b370560f652a8d4db8f679afea
Carlos Mendez <c.rmendez@gmail.com>
2022-08-12 11:27:07 -0500

    fixing cart

--- src/components/Cart/ViewCart.jsx
+++ src/components/Cart/ViewCart.jsx
@@ -1,84 +1,99 @@
-import React, {Fragment, useEffect} from "react";
-import { useSelector, useDispatch } from 'react-redux';
+import React, { Fragment, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import NavBar from "../NavBar/NavBar";
-import { deleteProductToCart, addProductToCart, deleteProduct } from "../../redux/actions/cart";
+import {
+  deleteProductToCart,
+  addProductToCart,
+  deleteProduct,
+} from "../../redux/actions/cart";
 
 const ViewCart = () => {
-    const dispatch = useDispatch();
-    const stateCart = useSelector((state) => state.cartReducer.cart)
-    const total = useSelector((state) => state.cartReducer.total)
+  const dispatch = useDispatch();
+  const stateCart = useSelector((state) => state.cartReducer.cart);
+  const total = useSelector((state) => state.cartReducer.total);
 
-    function productDelete(id) {
-        dispatch(deleteProduct(id))
-    }
-    function handlerDeleteToCart(id) {
-        dispatch(deleteProductToCart(id))
-    }
-    function add(product) {
-        let productDes = {
-            id: product.id,
-            name: product.name,
-            price: product.price,
-            image: product.image,
-            quantity: 1,
-          };
-        dispatch(addProductToCart(productDes))
-    }
+  function productDelete(id) {
+    dispatch(deleteProduct(id));
+  }
+  function handlerDeleteToCart(id) {
+    dispatch(deleteProductToCart(id));
+  }
+  function add(product) {
+    let productDes = {
+      id: product.id,
+      name: product.name,
+      price: product.price,
+      image: product.image,
+      quantity: 1,
+    };
+    dispatch(addProductToCart(productDes));
+  }
 
-    return (
-        <Fragment>
-            <NavBar />
-            <div className="mt-5 text-2xl">Index Carrito</div>
-            <br /><br />
-            <div className="sm:container md:mx-auto bg-[#e2e8f0]">
-                <div className="flex justify-center">
+  return (
+    <Fragment>
+      <NavBar />
+      <div className="mt-5 text-2xl">Index Carrito</div>
+      <br />
+      <br />
+      <div className="sm:container md:mx-auto bg-[#e2e8f0]">
+        <div className="flex justify-center">
+          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 ">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-40">
+              <tr>
+                <th scope="col" className="py-3 px-6">
+                  Product
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  Price
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  Quantity
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  Delete
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* <tr> */}
 
-                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 ">
-                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-40">
-                        <tr >
-                            <th scope="col" className="py-3 px-6">Product</th>
-                            <th scope="col" className="py-3 px-6">Price</th>
-                            <th scope="col" className="py-3 px-6">Quantity</th>
-                            <th scope="col" className="py-3 px-6">Delete</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {/* <tr> */}
-
-                            {
-                                stateCart.map((item,i)=>{
-                                    return (
-                                        <tr key={i} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                            <td className="py-4 px-6">{item.name}</td>
-                                            <td className="py-4 px-6">$ {item.price}.00</td>
-                                            <span className="m-5">
-                                                <button onClick={()=>handlerDeleteToCart(item.id)}>-</button>
-                                                <td className="py-4 px-6">{item.quantity}</td>
-                                                <button onClick={()=>add(item)}>+</button>
-                                            </span>
-                                            <td className="py-4 px-6">
-                                                <button onClick={()=>productDelete(item.id)}>X</button>
-                                            </td>
-                                        </tr>
-                                    )
-                                })
-                            }
-                        {/* </tr> */}
-                        <div>TOTAL: $ {total}</div>
-                    </tbody>
-                </table>
-                </div>
-                <Link to={`/product/carrito`}>
-                                    <button
-                                        className="absolute mt-8 bg-blue-500 hover:bg-blue-700 text-white font-bold w-32 py-2 rounded"
-                                    >
-                                          Buy now
-                                    </button>
-                </Link>
-            </div>
-        </Fragment>
-    )
-}
+              {stateCart.map((item, i) => {
+                return (
+                  <tr
+                    key={i}
+                    className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                  >
+                    <td className="py-4 px-6">{item.name}</td>
+                    <td className="py-4 px-6">$ {item.price}.00</td>
+                    <td>
+                      <button onClick={() => handlerDeleteToCart(item.id)}>
+                        -
+                      </button>
+                      <div className="py-4 px-6">{item.quantity}</div>
+                      <button onClick={() => add(item)}>+</button>
+                    </td>
+                    <td className="py-4 px-6">
+                      <button onClick={() => productDelete(item.id)}>X</button>
+                    </td>
+                  </tr>
+                );
+              })}
+              {/* </tr> */}
+              <tr>
+                <td>TOTAL: $ {total}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <Link to={`/product/carrito`}>
+          <button className="absolute mt-8 bg-blue-500 hover:bg-blue-700 text-white font-bold w-32 py-2 rounded">
+            Buy now
+          </button>
+        </Link>
+      </div>
+    </Fragment>
+  );
+};
 
 export default ViewCart;
--- src/components/Cart/getBuy.jsx
+++ src/components/Cart/getBuy.jsx
@@ -1,86 +1,86 @@
-import { CardElement, useStripe, useElements} from "@stripe/react-stripe-js"
-import { useSelector } from "react-redux";
-import axios from 'axios'
-import {useState} from 'react'
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import { useSelector, useDispatch } from "react-redux";
+import axios from "axios";
+import { useState } from "react";
 import Cart from "../Cart/Cart";
-
-
+import { resetCart } from "../../redux/actions/cart.js";
 
 const CheckoutForm = () => {
-  const stripe = useStripe()
-  const elements = useElements()
-  const [loading, setLoading] = useState(false)
+  const dispatch = useDispatch();
+  const stripe = useStripe();
+  const elements = useElements();
+  const [loading, setLoading] = useState(false);
   const stateCart = useSelector((state) => state.cartReducer.cart);
-
+  console.log(stateCart);
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
-    const {error, paymentMethod} = await stripe.createPaymentMethod({
-        type: 'card',
-        card: elements.getElement(CardElement)
-    })
-    setLoading(true)
+    e.preventDefault();
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: elements.getElement(CardElement),
+    });
+    setLoading(true);
     if (!error) {
-      const {id} = paymentMethod
-      const allQuantity = stateCart.length
-      var allToPay = 300
-      for (var i=0;i<stateCart.length;i++) allToPay = allToPay + stateCart[i].price
-      console.log(allToPay)
-      try{
-      const {data} = await axios.post('http://localhost:3001/api/checkout', {
-        id,
-        stateCart,
-        allQuantity,
-        allToPay
-      })
-      console.log(data)
-      elements.getElement(CardElement).clear()
-    }catch(error){
-      console.log(error)
+      const { id } = paymentMethod;
+      const allQuantity = stateCart.length;
+      var allToPay = 300;
+      for (var i = 0; i < stateCart.length; i++)
+        allToPay = allToPay + stateCart[i].price;
+      console.log(allToPay);
+      try {
+        const { data } = await axios.post(
+          "http://localhost:3001/api/checkout",
+          {
+            id,
+            stateCart,
+            allQuantity,
+            allToPay,
+          }
+        );
+        console.log(data);
+        elements.getElement(CardElement).clear();
+        dispatch(resetCart);
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
     }
-    setLoading(false)
-  }
-}
-  
+  };
+
   return (
     <div className="flex justify-center">
-      <div className="border-2 w-100 h-72 mt-24" >
-      <Cart stateCart={stateCart} />
-      <form onSubmit={handleSubmit} className=" w-96 h-64 mt-36 border-2">
-        <div className="">
-          <div className="flex justify-initial" >
-            Put date's of your card:
+      <div className="border-2 w-100 h-72 mt-24">
+        <Cart stateCart={stateCart} />
+        <form className=" w-96 h-64 mt-36 border-2">
+          <div className="">
+            <div className="flex justify-initial">Put date's of your card:</div>
+            <div>
+              <CardElement className="border-2" />
+            </div>
+            <button
+              onClick={handleSubmit}
+              type="button"
+              className="absolute mt-8 bg-blue-500 hover:bg-blue-700 text-white font-bold w-32 py-2 px-4 rounded"
+            >
+              Buy
+            </button>
           </div>
-          <div>
-            <CardElement  className="border-2"/>
-          </div>
-              <button 
-                
-                type="button" 
-                className="mt-8 bg-blue-500 hover:bg-blue-700 text-white font-bold w-32 py-2 px-4 rounded"
-                >
-                  Buy  
-              </button>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
 export default CheckoutForm;
 
-
-
-
-// {loading ? 
+// {loading ?
 //   (
-//     <svg 
-//       className="animate-spin h-5 w-5 mr-3 " 
+//     <svg
+//       className="animate-spin h-5 w-5 mr-3 "
 //       viewBox="0 0 24 24"
-//       >                          
-//       </svg> 
+//       >
+//       </svg>
 //       )
-//       : 
+//       :
 //       "buy"
-//       } 
+//       }
