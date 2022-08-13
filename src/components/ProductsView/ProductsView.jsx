f78f5f4944464b2daa6540f111dc1c61e7e9cb37
Alek30 <alemamk23k@gmail.com>
2022-08-13 10:56:50 -0300

    delete p-4

--- src/components/ProductsView/ProductsView.jsx
+++ src/components/ProductsView/ProductsView.jsx
@@ -3,14 +3,14 @@ import "flowbite";
 
 export default function ProductsView() {
   return (
-    <div className="flex space-x-5 md:container md:mx-auto mb-12 h-[68vh] bg-[#fbfbfccb] shadow-gray-900">
+    <div className="flex space-x-5 md:container md:mx-auto mb-24 mt-0 h-[68vh] bg-[#fbfbfccb] shadow-gray-900">
       <div className="min-h-screen bg-[#e2e8f0]flex pt-20">
         <div className="border-2 w-70 h-min rounded-md cursor-pointer shadow-lg overflow-hidden hover:shadow-xl transform hover:scale-105 duration-500">
           <img
             src="https://i.pinimg.com/originals/b7/4e/d1/b74ed19a92a174d85c3fa1879b1e361e.jpg"
             alt=""
           />
-          <div className="p-4 bg-white">
+          <div className=" bg-white">
             <span className="text-sm font-semibold text-red-50 bg-red-400 py-3 px-3 rounded-full">
               -30%
             </span>
@@ -24,7 +24,7 @@ export default function ProductsView() {
             src="https://ih1.redbubble.net/image.2245188445.9470/st,small,845x845-pad,1000x1000,f8f8f8.jpg"
             alt=""
           />
-          <div className="p-4 bg-white">
+          <div className=" bg-white">
             <span className="text-sm font-semibold text-red-50 bg-red-400 py-3 px-3 rounded-full">
               -30%
             </span>
@@ -38,7 +38,7 @@ export default function ProductsView() {
             src="https://cdn1.coppel.com/images/catalog/mkp/147/3000/1471001-1.jpg"
             alt=""
           />
-          <div className="p-4 bg-white">
+          <div className=" bg-white">
             <span className="text-sm font-semibold text-red-50 bg-red-400 py-3 px-3 rounded-full">
               -30%
             </span>
@@ -52,7 +52,7 @@ export default function ProductsView() {
             src="https://ih1.redbubble.net/image.3051489577.3866/poster,840x830,f8f8f8-pad,1000x1000,f8f8f8.jpg"
             alt=""
           />
-          <div className="p-4 bg-white">
+          <div className=" bg-white">
             <span className="text-sm font-semibold text-red-50 bg-red-400 py-3 px-3 rounded-full">
               -30%
             </span>
