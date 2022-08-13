5870b0520fc5edace57b6a95b16818e833080056
Alek30 <alemamk23k@gmail.com>
2022-08-12 22:16:45 -0300

    change div/h1 and title to Shopping Cart

--- src/components/Cart/ViewCart.jsx
+++ src/components/Cart/ViewCart.jsx
@@ -1,8 +1,12 @@
 import React, { Fragment, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { BsFillCartXFill } from "react-icons/bs";
 import NavBar from "../NavBar/NavBar";
 import Swal from "sweetalert2";
+import Footer from "../Footer/Footer.jsx";
+import "./ViewCart.css";
+import { NavLink } from "react-router-dom";
 import {
   deleteProductToCart,
   addProductToCart,
@@ -16,9 +20,6 @@ const ViewCart = () => {
 
   function productDelete(id) {
     dispatch(deleteProduct(id));
-  }
-  function handlerDeleteToCart(id) {
-    dispatch(deleteProductToCart(id));
 
     const Toast = Swal.mixin({
       toast: true,
@@ -36,6 +37,10 @@ const ViewCart = () => {
       icon: "warning",
       title: "The product was removed from the cart!",
     });
+  }
+
+  function handlerDeleteToCart(id) {
+    dispatch(deleteProductToCart(id));
   }
   function add(product) {
     let productDes = {
@@ -68,65 +73,84 @@ const ViewCart = () => {
   return (
     <Fragment>
       <NavBar />
-      <div className="mt-5 text-2xl">Index Carrito</div>
+      <h1>Shopping Cart</h1>
       <br />
       <br />
-      <div className="sm:container md:mx-auto bg-[#e2e8f0]">
-        <div className="flex justify-center">
-          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 ">
-            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-40">
-              <tr>
-                <th scope="col" className="py-3 px-6">
-                  Product
-                </th>
-                <th scope="col" className="py-3 px-6">
-                  Price
-                </th>
-                <th scope="col" className="py-3 px-6">
-                  Quantity
-                </th>
-                <th scope="col" className="py-3 px-6">
-                  Delete
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {/* <tr> */}
+      {stateCart.length > 0 ? (
+        <div className="sm:container md:mx-auto bg-[#e2e8f0]">
+          <div className="flex justify-center">
+            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 ">
+              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-40">
+                <tr>
+                  <th scope="col" className="py-3 px-6">
+                    Product
+                  </th>
+                  <th scope="col" className="py-3 px-6">
+                    Price
+                  </th>
+                  <th scope="col" className="py-3 px-6">
+                    Quantity
+                  </th>
+                  <th scope="col" className="py-3 px-6">
+                    Delete
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* <tr> */}
 
-              {stateCart.map((item, i) => {
-                return (
-                  <tr
-                    key={i}
-                    className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
-                  >
-                    <td className="py-4 px-6">{item.name}</td>
-                    <td className="py-4 px-6">$ {item.price}.00</td>
-                    <td>
-                      <button onClick={() => handlerDeleteToCart(item.id)}>
-                        -
-                      </button>
-                      <div className="py-4 px-6">{item.quantity}</div>
-                      <button onClick={() => add(item)}>+</button>
-                    </td>
-                    <td className="py-4 px-6">
-                      <button onClick={() => productDelete(item.id)}>X</button>
-                    </td>
-                  </tr>
-                );
-              })}
-              {/* </tr> */}
-              <tr>
-                <td>TOTAL: $ {total}</td>
-              </tr>
-            </tbody>
-          </table>
+                {stateCart.map((item, i) => {
+                  return (
+                    <tr
+                      key={i}
+                      className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                    >
+                      <td className="py-4 px-6">{item.name}</td>
+                      <td className="py-4 px-6">$ {item.price}.00</td>
+                      <td>
+                        <button onClick={() => handlerDeleteToCart(item.id)}>
+                          -
+                        </button>
+                        <div className="py-4 px-6">{item.quantity}</div>
+                        <button onClick={() => add(item)}>+</button>
+                      </td>
+                      <td className="py-4 px-6">
+                        <button onClick={() => productDelete(item.id)}>
+                          X
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+                {/* </tr> */}
+                <tr>
+                  <td>TOTAL: $ {total}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <Link to={`/product/carrito`}>
+            <button className="absolute mt-8 bg-blue-500 hover:bg-blue-700 text-white font-bold w-32 py-2 rounded">
+              Buy now
+            </button>
+          </Link>
         </div>
-        <Link to={`/product/carrito`}>
-          <button className="absolute mt-8 bg-blue-500 hover:bg-blue-700 text-white font-bold w-32 py-2 rounded">
-            Buy now
-          </button>
-        </Link>
-      </div>
+      ) : (
+        <div className="div_no_cartItem">
+          <div className="icon_cartEmpty">
+            <BsFillCartXFill />
+          </div>
+          <div>
+            <h4>Shopping cart empty</h4>
+          </div>
+          <div>
+            <NavLink className="keepShopping" to="/home">
+              ¡Keep Shopping!
+            </NavLink>
+          </div>
+        </div>
+      )}
+      <Footer />
     </Fragment>
   );
 };
