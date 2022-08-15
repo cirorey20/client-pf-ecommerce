1dc0b7ec24d5e53e075130da0048bb9f7b4fe061
Carlos Mendez <c.rmendez@gmail.com>
2022-08-15 16:57:28 -0500

    task done

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -5,11 +5,12 @@ import { detailProduct } from "../../redux/actions/products";
 import NavBar from "../NavBar/NavBar";
 import Footer from "../Footer/Footer.jsx";
 import { Link } from "react-router-dom";
-import Cart from "../Cart/Cart";
+//import Cart from "../Cart/Cart";
 import { addProductToCart } from "../../redux/actions/cart";
 
 const Details = () => {
-  const stateCart = useSelector((state) => state.cartReducer.cart);
+  //  const stateCart = useSelector((state) => state.cartReducer.cart);
+  const userLogin = useSelector((state) => state.authReducer.userLogin);
   const dispatch = useDispatch();
   const details = useSelector((state) => state.productReducer.productDetail);
   let { id } = useParams();
@@ -27,9 +28,9 @@ const Details = () => {
       quantity: 1,
     };
     dispatch(addProductToCart(productDes));
-    history('/cart')
+    history("/cart");
   }
-
+  console.log(userLogin?.user?.name);
   return (
     <Fragment>
       <NavBar />
@@ -43,11 +44,8 @@ const Details = () => {
       <br />
 
       <div className=" ">
-
         <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
           <div className="grid gap-5 row-gap-8 lg:grid-cols-2">
-
-
             <div>
               <img
                 className="object-cover w-full h-56 rounded shadow-lg sm:h-96"
@@ -59,28 +57,20 @@ const Details = () => {
               <div className="max-w-xl mb-6">
                 <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
                   ${details.price}
-
                   <br className="hidden md:block" />
                 </h2>
-                <p className="text-base text-gray-700 md:text-lg">{details.description}</p>
-
+                <p className="text-base text-gray-700 md:text-lg">
+                  {details.description}
+                </p>
               </div>
               <div className="grid gap-5 row-gap-8 sm:grid-cols-2">
                 <div className="bg-white border-l-4 shadow-sm border-deep-purple-accent-400">
                   <div className="h-full p-5 border border-l-0 rounded-r">
-                    <h6 className="mb-2 font-semibold leading-5">
-                      Rating:
-                    </h6>
-                    <p className="text-sm text-gray-900">
-                      ★★★★★
-                    </p>
+                    <h6 className="mb-2 font-semibold leading-5">Rating:</h6>
+                    <p className="text-sm text-gray-900">★★★★★</p>
                   </div>
                 </div>
-                <div className=" flex-none  m-2 w-40 h-100 right-40 float-left">
-                  <div className=" rounded-xl shadow-2xl p-8 h-40">
-                    <Cart stateCart={stateCart} />
-                  </div>
-                </div>
+
                 <div className="bg-white ">
                   <div className=" h-full p-5 ">
                     <button
@@ -89,20 +79,19 @@ const Details = () => {
                     >
                       Add Cart
                     </button>
-                    <Link to={`/product/carrito`}>
-                      <button
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                      >
-                        Buy
-                      </button>
-
-                    </Link>
+                    {userLogin?.user?.name ? (
+                      <Link to={`/product/carrito`}>
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                          Buy
+                        </button>
+                      </Link>
+                    ) : (
+                      ""
+                    )}
                   </div>
                 </div>
               </div>
             </div>
-
-
           </div>
         </div>
       </div>
