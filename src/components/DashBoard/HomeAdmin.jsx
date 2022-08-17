865b82f9aa2bfe6cef5483932877c1b3353178fe
cirorey20 <cirorey95@gmail.com>
2022-08-17 16:30:27 -0300

    changed component homeadmin

--- src/components/DashBoard/HomeAdmin.jsx
+++ src/components/DashBoard/HomeAdmin.jsx
@@ -1,8 +1,17 @@
 import React, { Fragment } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { logout } from "../../redux/actions/auth";
 import  "../../assets/tailwind.css";
 
 const HomeAdmin = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const onLogout = () => {
+        dispatch(logout());
+        navigate("/login");
+    };
     return (
         <Fragment>
             <div className="lg:mx-auto px-2 sm:px-6 lg:px-8">
@@ -10,7 +19,7 @@ const HomeAdmin = () => {
                 <div className="flex justify-between p-5">
                     <div className="text-sky-400 ">
                         {
-                        <Link to={"/"}>
+                        <Link to={"/admin/home"}>
                             <img className="" width={150} src={'https://upload.wikimedia.org/wikipedia/commons/4/4f/Universal-Pictures-Logo.svg'} alt="" />
                         </Link>
                         }
@@ -23,7 +32,7 @@ const HomeAdmin = () => {
                     <div className="float-right">
                         <span className="flex flex-nowrap pt-5">
                             <p className="text-2xl mt-2">nameadmin</p>
-                            <button className="text-1xl ml-5 text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 ">logout</button>
+                            <button onClick={()=>onLogout()} className="text-1xl ml-5 text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 ">logout</button>
                         </span>
                     </div>
                 </div>
@@ -35,30 +44,31 @@ const HomeAdmin = () => {
                 </svg>
                 </div>
             </div> */}
-            <div className="m-20 flex-initial w-full md:container md:mx-auto bg-[#e2e8f0] rounded-xl shadow-lg">
-                <div className="flex justify-evenly ">
+            <br /><br /><br />
+            <div className="m-20 w-full md:container md:mx-auto bg-[#e2e8f0] rounded-xl shadow-lg">
+                <div className="flex flex-row flex-wrap justify-center content-center ">
 
                     <div className="grid grid-cols-3 gap-4 content-center p-10">
 
-                        <div className="bg-purple-700">
-                            <Link to={'/product/dashBoard'} className="bg-purple-700 block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                                <h5 className="mb-2 text-teal-50 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">PRODUCTS</h5>
+                        <div className="bg-purple-700 ">
+                            <Link to={'/product/dashBoard'} className="flex flex-row flex-wrap justify-center content-center h-40 bg-purple-700 block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                                <h5 className=" mb-2 text-teal-50 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">PRODUCTS</h5>
+                            </Link>
+                        </div>
+                        <div className="bg-purple-700 ">
+                            <Link to={'/admin/home'} className="flex flex-row flex-wrap justify-center content-center h-40 bg-purple-700 block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                                <h5 className="mb-2 text-teal-50 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">USERS</h5>
                             </Link>
                         </div>
                         <div className="bg-purple-700">
-                            <a href="#" className="bg-purple-700 block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                                <h5 className="mb-2 text-teal-50 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">USERS</h5>
-                            </a>
-                        </div>
-                        <div className="bg-purple-700">
-                            <a href="#" className="bg-purple-700 block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                            <Link to={'/admin/home'} className="flex flex-row flex-wrap justify-center content-center h-40 bg-purple-700 block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                                 <h5 className="mb-2 text-teal-50 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">MANAGERS ORDERS</h5>
-                            </a>
+                            </Link>
                         </div>
-                        <div className="bg-purple-700">
-                            <a href="#" className="bg-purple-700 block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                        <div className="bg-purple-700 ">
+                            <Link to={'/admin/home'} className="flex flex-row flex-wrap justify-center content-center h-40 bg-purple-700 block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                                 <h5 className="mb-2 text-teal-50 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">CATEGORIES</h5>
-                            </a>
+                            </Link>
                         </div>
                     </div>
 
