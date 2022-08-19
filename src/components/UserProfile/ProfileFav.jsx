e13a5ac2652a2b7c80ace016abb35262ae66a2c1
Alek30 <alemamk23k@gmail.com>
2022-08-18 23:27:19 -0300

    add import/ProfileFav

---
+++ src/components/UserProfile/ProfileFav.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Favorite from "../Favoritos/Favorite";
+import { useSelector } from "react-redux";
+
+const ProfileFav = () => {
+  const favorites = useSelector((state) => state.wishlistReducer);
+
+  console.log(favorites);
+  return (
+    <div>
+      <h2>Favoritos</h2>
+      <div>
+        {favorites.map((fav) => {
+          return <Favorite key={fav.id} favorite={fav} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default ProfileFav;
