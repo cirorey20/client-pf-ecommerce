10f17563e125f579f863a12eb93f3238fc85392a
Alek30 <alemamk23k@gmail.com>
2022-08-19 17:06:48 -0300

    add wis list/button back

--- src/components/UserProfile/ProfileFav.jsx
+++ src/components/UserProfile/ProfileFav.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import Favorite from "../Favoritos/Favorite";
 import { useSelector } from "react-redux";
 
-const ProfileFav = () => {
+const ProfileFav = ({ toggleFav }) => {
   const favorites = useSelector((state) => state.wishlistReducer);
 
-  console.log(favorites);
   return (
     <div>
-      <h2>Favoritos</h2>
-      <div>
+      <button
+        className="flex justify-center py-2 px-4 ml-5 mt-5 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        onClick={toggleFav}
+      >
+        Back
+      </button>
+      <h1>Wish List</h1>
+      <div className="flex gap-5 flex-wrap justify-center">
         {favorites.map((fav) => {
           return <Favorite key={fav.id} favorite={fav} />;
         })}
