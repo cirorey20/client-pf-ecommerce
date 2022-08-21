3cd00aa5636ce56f921a4d5eb55f340024fe1bca
Alek30 <alemamk23k@gmail.com>
2022-08-21 12:33:52 -0300

    add styles bg/shadow

---
+++ src/components/Favoritos/EditForm.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+const EditForm = ({ onSubmit, editProfile, toggleEdit }) => {
+  return (
+    <div className="absolute px-24 border shadow-lg rounded-lg ring-offset-2 ring-2 left-2/4 top-2/4 -translate-x-2/4 -translate-y-2/4  bg-white">
+      <form action="" onSubmit={onSubmit} className="w-full max-w-sm my-5 ">
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label
+              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+              for="inline-full-name"
+            >
+              Name
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <input
+              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2  text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+              id="inline-full-name"
+              type="text"
+              name="name"
+              onChange={editProfile}
+            />
+          </div>
+        </div>
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label
+              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+              for="inline-password"
+            >
+              LastName
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <input
+              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+              type="text"
+              name="lastName"
+              onChange={editProfile}
+            />
+          </div>
+        </div>
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3"></div>
+        </div>
+        <div className="md:flex md:items-center">
+          <div className="md:w-1/3"></div>
+          <div className="md:w-2/3 flex gap-5">
+            <button
+              className="shadow bg-red-300 hover:bg-slate-900 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+              onClick={(e) => toggleEdit(e)}
+            >
+              Cancel
+            </button>
+            <button
+              className="shadow bg-slate-700 hover:bg-slate-900 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+              type="submit"
+            >
+              Confirm
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default EditForm;
