4584ed4eb4e12c5c9a78a9e588782ca987552f6f
Carlos Mendez <c.rmendez@gmail.com>
2022-08-24 19:41:37 -0500

    home admin redesign

--- src/components/Admin/HomeAdmin.jsx
+++ src/components/Admin/HomeAdmin.jsx
@@ -4,16 +4,31 @@ import { Link } from "react-router-dom";
 import "./homeAdmin.css";
 
 const HomeAdmin = () => {
-
-    return (
-        <div className="contenParehnt">
-
-            <div className="contenFull relative">
-                <NavAdmin section={'Admin'}/>
-
-                <br /><br /><br />
-                <div className="containerBox z-1 ">
-                    <Link to={'/product/dashBoard'} className="">
+  return (
+    <div>
+      <NavAdmin />
+      <div className="admin_container">
+        <div className="admin_option">
+          <Link to={"/product/dashBoard"} className="">
+            <h5>PRODUCTS</h5>
+          </Link>
+        </div>
+        <div className="admin_option">
+          <Link to={"/users/dashboard"} className="">
+            <h5>USERS</h5>
+          </Link>
+        </div>
+        <div className="admin_option">
+          <Link to={"/orders"} className="">
+            <h5>ORDERS</h5>
+          </Link>
+        </div>
+        <div className="admin_option">
+          <Link to={"/product/categories"} className="">
+            <h5>CATEGORIES</h5>
+          </Link>
+        </div>
+        {/* <Link to={'/product/dashBoard'} className="">
                         <div className="h-60 w-60 bg-[#1B0FC5] hover:bg-violet-800 m-5 rounded-lg grid justify-items-center content-center">
                             <h5 className="  mb-2 text-2xl font-bold tracking-tight text-white">PRODUCTS</h5>
                         </div>
@@ -32,20 +47,10 @@ const HomeAdmin = () => {
                         <div className="h-60 w-60 bg-[#1B0FC5] hover:bg-violet-800 m-5 rounded-lg grid justify-items-center content-center">
                             <h5 className="  mb-2 text-2xl font-bold tracking-tight text-white">CATEGORIES</h5>
                         </div>
-                    </Link>
-                </div>
-
-            </div>
-                <div className="static wave  ">
-                    <div className="relative bottom-15 left-0">
-
-                        <svg className="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                            <path fill="#e7008a" fill-opacity="1" d="M0,288L80,261.3C160,235,320,181,480,149.3C640,117,800,107,960,112C1120,117,1280,139,1360,149.3L1440,160L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
-                        </svg>
-                    </div>
-                </div>
-        </div>
-    )
-}
+                    </Link> */}
+      </div>
+    </div>
+  );
+};
 
 export default HomeAdmin;
